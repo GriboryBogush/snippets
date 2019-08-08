fb48c3cd5eaef4588ae97d0094a30fbdc056110f
Grigory Bogush <g.bogush@leviossa.net>
2019-08-08 21:42:35 +0300

    inital commit

---
+++ basic_maths.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+  float f = 0.0;
+
+  std::cout << "Please enter a number to sqrt" << std::endl;
+  std::cin >> f;
+  std::cout << sqrt(f) << std::endl;
+
+}
---
+++ input_output.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+
+int main() {
+  float f;
+  std::string s;
+  std::cin >> f >> s;
+
+  std::cout << s << " " << f << std::endl;
+}
