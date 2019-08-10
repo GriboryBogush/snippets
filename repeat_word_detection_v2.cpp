3d844f37aedd8a6dc8a9757ddefd494fd9495c5a
Grigory Bogush <g.bogush@leviossa.net>
2019-08-10 11:36:30 +0300

    v2 of same word program

---
+++ repeat_word_detection_v2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+int main() {
+  int number_of_words = 0;
+  std::string previous = "";
+  std::string current;
+  while (std::cin >> current) {
+    ++number_of_words;
+    if (previous == current)
+        std::cout << "word number: " << number_of_words
+	          << " repeated word: " << current << std::endl;
+    previous = current;
+  }
+}
